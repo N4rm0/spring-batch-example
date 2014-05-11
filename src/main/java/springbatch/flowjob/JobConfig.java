e882a9c6436d48f8721d2b28a2b6ab6449bbaab2
Monard Vong <travelingsoul86@gmail.com>
2014-05-12 01:12:02 +0200

    try with h2 database

--- src/main/java/springbatch/flowjob/JobConfig.java
+++ src/main/java/springbatch/flowjob/JobConfig.java
@@ -1,66 +1,77 @@
 package springbatch.flowjob;
 
-import javax.sql.DataSource;
-
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@EnableBatchProcessing
 // annotation to autoconfigure jobbuilders stepbuilders.
 public class JobConfig {
 
-	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
+	private PlatformTransactionManager transactionManager = new ResourcelessTransactionManager();
 
-	@Autowired
-	private StepBuilderFactory stepBuilderFactory;
-
-	@Bean(destroyMethod = "shutdown")
-	public DataSource dataSource() {
-		// Spring Batch requires a data-source to log execution details, so
-		// create a h2 memory data source
-		final EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-		return builder.setType(EmbeddedDatabaseType.H2).build();
+	@Bean
+	public JobBuilderFactory jobBuilderFactory() {
+		return new JobBuilderFactory(jobRepository());
 	}
 
 	@Bean
+	public StepBuilderFactory stepBuilderFactory() {
+		return new StepBuilderFactory(jobRepository(), transactionManager);
+	}
+
+	@Bean
+	public JobRepository jobRepository() {
+		try {
+			return new MapJobRepositoryFactoryBean(transactionManager)
+					.getJobRepository();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	// @Bean(destroyMethod = "shutdown")
+	// public DataSource dataSource() {
+	// // Spring Batch requires a data-source to log execution details, so
+	// // create a h2 memory data source
+	// final EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+	// return builder.setType(EmbeddedDatabaseType.H2).build();
+	// }
+
+	@Bean
 	public Job job() {
-		return jobBuilderFactory.get("job").flow(stepA()).on("FAILED")
+		return jobBuilderFactory().get("job").flow(stepA()).on("FAILED")
 				.to(stepB()).from(stepA()).on("*").to(stepC()).end().build();
 	}
 
 	@Bean
 	public Step stepA() {
-		return stepBuilderFactory.get("stepA")
+		return stepBuilderFactory().get("stepA")
 				.tasklet(new RandomFailTasket("stepA")).build();
 	}
 
 	@Bean
 	public Step stepB() {
-		return stepBuilderFactory.get("stepB")
+		return stepBuilderFactory().get("stepB")
 				.tasklet(new PrintTextTasklet("stepB")).build();
 	}
 
 	@Bean
 	public Step stepC() {
-		return stepBuilderFactory.get("stepC")
+		return stepBuilderFactory().get("stepC")
 				.tasklet(new PrintTextTasklet("stepC")).build();
 	}
 
@@ -72,14 +83,12 @@ public class JobConfig {
 		// get the job config bean (i.e this bean)
 		final JobConfig jobConfig = appContext.getBean(JobConfig.class);
 		// get the job launcher by
-		final JobLauncher launcher = appContext.getBean(JobLauncher.class);
-
+		JobRepository repo = jobConfig.jobRepository();
 		try {
 			// launch the job
-			launcher.run(jobConfig.job(), new JobParameters());
+			repo.createJobExecution("job", new JobParameters());
 		} catch (JobExecutionAlreadyRunningException | JobRestartException
-				| JobInstanceAlreadyCompleteException
-				| JobParametersInvalidException e) {
+				| JobInstanceAlreadyCompleteException e) {
 			e.printStackTrace();
 		}
 	}
