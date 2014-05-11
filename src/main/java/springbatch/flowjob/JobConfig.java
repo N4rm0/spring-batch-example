2eb433a70d80a70c413a42ee4bd26ff4cb003db7
Monard Vong <travelingsoul86@gmail.com>
2014-05-11 23:37:45 +0200

    proper job configuration

--- src/main/java/springbatch/flowjob/JobConfig.java
+++ src/main/java/springbatch/flowjob/JobConfig.java
@@ -10,72 +10,48 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableBatchProcessing
-@ComponentScan
 public class JobConfig {
 
 	@Autowired
-	private StepBuilderFactory stepBuilder;
+	private JobBuilderFactory jobBuilderFactory;
 
 	@Autowired
-	private MapJobRepositoryFactoryBean jobRepositoryFactory;
-
-	@Bean
-	public StepBuilderFactory getStepBuilder(
-			PlatformTransactionManager transactionManager) {
-		return new StepBuilderFactory(jobRepository(), transactionManager);
-	}
-
-	@Bean
-	public JobBuilderFactory getJobBuilder() {
-		return new JobBuilderFactory(jobRepository());
-	}
-
-	@Bean
-	public JobRepository jobRepository() {
-		try {
-			return jobRepositoryFactory.getJobRepository();
-		} catch (Exception e) {
-			return null;
-		}
-	}
+	private StepBuilderFactory stepBuilderFactory;
 
 	@Bean
 	public Job job() {
-		return jobBuilder.get("job").flow(stepA()).on("FAILED").to(stepB())
-				.from(stepA()).on("*").to(stepC()).end().build();
+		return jobBuilderFactory.get("job").flow(stepA()).on("FAILED")
+				.to(stepB()).from(stepA()).on("*").to(stepC()).end().build();
 	}
 
 	@Bean
 	public Step stepA() {
-		return stepBuilder.get("stepA").tasklet(new RandomFailTasket("stepA"))
-				.build();
+		return stepBuilderFactory.get("stepA")
+				.tasklet(new RandomFailTasket("stepA")).build();
 	}
 
 	@Bean
 	public Step stepB() {
-		return stepBuilder.get("stepB").tasklet(new PrintTextTasklet("stepB"))
-				.build();
+		return stepBuilderFactory.get("stepB")
+				.tasklet(new PrintTextTasklet("stepB")).build();
 	}
 
 	@Bean
 	public Step stepC() {
-		return stepBuilder.get("stepC").tasklet(new PrintTextTasklet("stepC"))
-				.build();
+		return stepBuilderFactory.get("stepC")
+				.tasklet(new PrintTextTasklet("stepC")).build();
 	}
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		ApplicationContext appContext = new AnnotationConfigApplicationContext(
 				JobConfig.class);
@@ -83,9 +59,8 @@ public class JobConfig {
 		JobConfig jobConfig = appContext.getBean(JobConfig.class);
 		JobLauncher launcher = appContext.getBean(JobLauncher.class);
 
-		Job job = jobConfig.job();
 		try {
-			launcher.run(job, new JobParameters());
+			launcher.run(jobConfig.job(), new JobParameters());
 		} catch (JobExecutionAlreadyRunningException | JobRestartException
 				| JobInstanceAlreadyCompleteException
 				| JobParametersInvalidException e) {
