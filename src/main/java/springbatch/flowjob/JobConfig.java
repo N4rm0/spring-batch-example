130022a6cc54011aabe775ad6794c2343440a51c
Monard Vong <travelingsoul86@gmail.com>
2014-05-22 00:47:21 +0200

    example of configuration with @EnableBatchProcessing

--- src/main/java/springbatch/flowjob/JobConfig.java
+++ src/main/java/springbatch/flowjob/JobConfig.java
@@ -4,6 +4,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.JobLauncher;
@@ -14,6 +15,7 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
 import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -21,25 +23,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-// do not use @EnableBatchProcessing, we set up manually for rapid prototyping
+@EnableBatchProcessing
 public class JobConfig {
 
-	private PlatformTransactionManager transactionManager = new ResourcelessTransactionManager();
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public JobBuilderFactory jobBuilderFactory() {
-		return new JobBuilderFactory(jobRepository());
-	}
-
-	@Bean
-	public StepBuilderFactory stepBuilderFactory() {
-		return new StepBuilderFactory(jobRepository(), transactionManager);
+	public PlatformTransactionManager transactionManager() {
+		return new ResourcelessTransactionManager();
 	}
 
 	@Bean
 	public JobRepository jobRepository() {
 		try {
-			return new MapJobRepositoryFactoryBean(transactionManager)
+			return new MapJobRepositoryFactoryBean(transactionManager())
 					.getJobRepository();
 		} catch (Exception e) {
 			return null;
@@ -55,26 +56,26 @@ public class JobConfig {
 
 	@Bean
 	public Job job() {
-		return jobBuilderFactory().get("job").
+		return jobBuilderFactory.get("job").
 				flow(stepA()).on("FAILED").to(stepC()).
 				from(stepA()).on("*").to(stepB()).next(stepC()).end().build();
 	}
 
 	@Bean
 	public Step stepA() {
-		return stepBuilderFactory().get("stepA")
+		return stepBuilderFactory.get("stepA")
 				.tasklet(new RandomFailTasket("stepA")).build();
 	}
 
 	@Bean
 	public Step stepB() {
-		return stepBuilderFactory().get("stepB")
+		return stepBuilderFactory.get("stepB")
 				.tasklet(new PrintTextTasklet("stepB")).build();
 	}
 
 	@Bean
 	public Step stepC() {
-		return stepBuilderFactory().get("stepC")
+		return stepBuilderFactory.get("stepC")
 				.tasklet(new PrintTextTasklet("stepC")).build();
 	}
 
